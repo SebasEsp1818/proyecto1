76203231c69fcb5ec25bb543574379b89ad98f99
sebasesp1818 <sebastianespinozaguti@gmail.com>
2024-05-28 10:16:22 -0600

    Estructura condicional

---
+++ src/Estructura_condicional.kt
@@ -0,0 +1,74 @@
+fun main() {
+    /*
+    //Problema 1
+    print("Ingrese el sueldo del empleado.")
+    val sueldo: Double = readln().toDouble()
+
+    //condicional if , si sueldo es mayor 3000,
+    //imprimir "Debe pagar impuestos
+    if (sueldo > 3000) {
+        println("Debe pagar impuestos")
+    }
+
+
+
+*/
+    /*
+    //Problema 2
+    print("Ingrese primer valor:")
+    val valor1 = readln().toInt()
+    print("Ingrese segundo valor:")
+    val valor2 = readln().toInt()
+    //condicional con if / else
+    if (valor1 > valor2) {
+        print("El mayor valor es $valor1")
+    } else {
+        print("El mayor valor es $valor2")
+    }
+    */
+    /*
+    //Problema 3
+    print("Ingrese el primer valor:")
+    val valor1: Int = readln(). toInt()
+    print("Ingrese el segundo valor:")
+    val valor2: Int = readln().toInt()
+    if (valor1 < valor2) {
+        val suma: Int = valor1 + valor2
+        val resta: Int = valor1 - valor2
+        println("La suma de los dos valores es $suma")
+        println("La resta de los dos valores es $resta")
+    }else{
+        val producto: Int = valor1 * valor2
+        val division: Int = valor1 / valor2
+        println("El producto de los dos vectores es $producto")
+        println("La division de los valores es : $division")
+
+ */
+    /*
+//Problema 4
+print("Ingrese la primera nota:")
+val valor1: Int = readln().toInt()
+    print("Ingrese la segunda nota")
+    val valor2: Int = readln().toInt()
+    print("Ingrese la tercera nota")
+    val valor3: Int = readln().toInt()
+    var promedio: Int
+    promedio = valor1 + valor2 + valor3 / 3
+println("El promedio es de $promedio")
+    if (promedio > 70)
+        println("Promocionado")
+
+*/
+//Problema 5
+
+    println("cuantos digitos tiene tu numero?")
+    val numero = readln().toInt()
+    if (numero >= 10) {
+        println("Tu numero tiene 2 digitos")
+    } else {
+        println("El numero contiene un digito")
+    }
+}
+
+
+
